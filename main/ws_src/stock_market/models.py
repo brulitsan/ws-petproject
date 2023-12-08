c2f93607166e469f51aeda0bf1c74be3a8fca187
matve <matveytulaevm.ama@gmail.com>
2023-12-08 06:43:46 +0300

    task#2 add models + fix

--- main/ws_src/stock_market/models.py
+++ main/ws_src/stock_market/models.py
@@ -1,10 +1,26 @@
 import uuid
 
 from django.db import models
-from ws_src.common.models import BaseOrderType, ProductCategories
+from ws_src.common.choices import BaseOrderType, BaseProductType
+
+
+class ProductCategories(models.Model):
+    name = models.CharField(max_length=30, choices=BaseProductType.choices)
+    parent = models.ForeignKey("self", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return (
+            f"product - {self.name} | parent - {self.parent}"
+            if self.parent
+            else self.name
+        )
 
 
 class Product(models.Model):
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.ForeignKey(ProductCategories, on_delete=models.PROTECT, default="")
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -12,10 +28,6 @@ class Product(models.Model):
     min_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "Product"
-        verbose_name_plural = "Products"
 
     def __str__(self):
         return f"product - {self.name}"
@@ -26,4 +38,4 @@ class Order(models.Model):
     user_id: models.OneToOneField("users.User", on_delete=models.PROTECT)
     price = models.DecimalField(decimal_places=3, max_digits=3)
     currency = models.CharField(max_length=20)
-    type = models.CharField(choices=BaseOrderType.choices(), max_length=30)
+    type = models.CharField(choices=BaseOrderType.choices, max_length=30)
--- main/ws_src/users/models.py
+++ main/ws_src/users/models.py
@@ -1,10 +1,11 @@
 import uuid
 
 from django.db import models
-from ws_src.common.models import BaseUserTypes
+from ws_src.common.choices import BaseUserTypes
+from django.contrib.auth.models import AbstractUser, Permission, Group
 
 
-class User(models.Model):
+class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = (
         models.CharField(
@@ -13,20 +14,39 @@ class User(models.Model):
             help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
         ),
     )
-    role = models.CharField(choices=BaseUserTypes.choices(), max_length=20)
+    role = models.CharField(choices=BaseUserTypes.choices, max_length=20)
     first_name = models.CharField(max_length=150, blank=True)
     user_balance = models.DecimalField(decimal_places=3, max_digits=3)
     last_name = models.CharField(max_length=150, blank=True)
     email = models.EmailField(blank=True, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=("groups"),
+        blank=True,
+        help_text=(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        related_name="custom_user_set",
+        related_query_name="user",
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=("user permissions"),
+        blank=True,
+        help_text=("Specific permissions for this user."),
+        related_name="custom_user_set",
+        related_query_name="user",
+    )
 
     def __str__(self):
         return f"username {self.username} | user id - {self.id}"
 
 
 class UserProfile(models.Model):
-    username = models.CharField(max_length=50)
     id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
     about = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
